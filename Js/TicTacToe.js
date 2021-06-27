95a9aab189c34c51cf58a079d54caaa7ca22cafa
SamVerwoerd <302324869@student.rocmondriaan.nl>
2021-06-28 00:42:10 +0200

    more //added

--- Js/TicTacToe.js
+++ Js/TicTacToe.js
@@ -1,14 +1,5 @@
 import Player from "./Player.js";
 
-
-
-/**
- * To start:
- * Check index.html, make sure the board with fields are created with correct classnames
- * The board should be 3x3 fields
- * Add correct classes
- */
-
 const players = [];
 let currentPlayer = 0; // This is the index of the array of the currentplayer
 const fields = document.querySelectorAll('.board > .field');
@@ -18,11 +9,7 @@ const resetButton = document.querySelector(".reset-btn");
  const playerTwo = new Player("Player 2", 'O');
 //Add both players to the players array
  players.push(playerOne, playerTwo);
-/**
- * Assignment
- * Make a loop thru all the fields and add a click event. 
- * Connect the addSymbolToField function in the eventHandler
- */
+
 
 //adding symbol to the field
 for (let i = 0; i < fields.length; i++) {
@@ -50,27 +37,18 @@ for (let i = 0; i < fields.length; i++) {
     })
 }
 
-/**
- * Assignment 
- * Give body to the reset function (the function exists below)
- * 
- */
 
 resetButton.addEventListener("click", resetGame);
+
 
 function addSymbolToField(field) {
     console.log(field)
     const fieldContent = field.textContent;
+    //Checks if the field is already in use
     if (fieldContent === 'X' || fieldContent === 'O') {
         alert('This field can not be used');
         return;
     }
-
-    /**
-     * Assignment
-     * Add the current player symbol to the field textContent
-     * What more needs to be done here? Make a short todolist
-     */
 
     return true;
 }
